01091a20404a3ab950a6d8fc4ced17ba5d5f049f
soorek <soorek.yellow@gmail.com>
2025-02-23 15:31:46 +0300

    Добавьте файлы проекта.

---
+++ OOP/Cat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Cat
+    {
+        public string name;
+        public int age;
+        public double height;
+        public string color;
+        public string poroda;
+
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,19 @@
+﻿using OOP;
+
+Cat murka = new Cat();
+Cat moska = new Cat();
+
+
+murka.age = 5;
+murka.name = "Murka";
+murka.color = "белая";
+murka.poroda = "сиамская";
+murka.height = 30;
+
+moska.age = 9;
+moska.name = "moska";
+moska.color = "черная";
+murka.poroda = "бенгальская";
+moska.height = 35;
+
+
